a24ad7d5f1b45ced37a23d3e6caab67fd86929ec
eng-alimallouhe <engalimallouhe@gmail.com>
2025-06-20 21:11:54 +0300

    build(domain-layer): the interfaces for repository and specification pattern have been added

---
+++ LMS/LMS.Domain/Abstractions/Specifications/IProjectedSpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace LMS.Domain.Abstractions.Specifications
+{
+    public interface IProjectedSpecification<TEntity, TResult, TKey> :
+        ISpecification<TEntity>
+        where TEntity : class
+    {
+        Expression<Func<TEntity, TKey>> GroupBy { get; }
+        Expression<Func<IGrouping<TKey, TEntity>, TResult>> Selector { get; }
+        Func<IQueryable<TResult>, IOrderedQueryable<TResult>>? ResultOrdering { get; }
+    }
+}
---
+++ LMS/LMS.Domain/Abstractions/Specifications/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace LMS.Domain.Abstractions.Specifications
+{
+    public interface ISpecification<TEntity> 
+        where TEntity : class
+    {
+        Expression<Func<TEntity, bool>>? Criteria { get; }
+        List<string> Includes { get; }
+        bool IsTrackingEnabled { get; }
+        Expression<Func<TEntity, object>>? OrderBy { get; }
+        Expression<Func<TEntity, object>>? OrderByDescending { get; }
+        int? Skip { get; }
+        int? Take { get; }
+    }
+}
